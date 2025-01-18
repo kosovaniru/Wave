9cf6f07e736ea56462d966e3946e02c9f3a5c32c
Iryna Kosovan <kosovaniryne@gmail.com>
2025-01-18 17:18:46 +0200

    finished all sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,15 @@
+$(function() {
+  $('.blog__slider').slick({
+    dots: true,
+  });
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+      $('.header__inner').addClass('active')
+    } else {
+      $('.header__inner').removeClass('active')
+    }
+  })
+});
 
 const mixer = mixitup('.gallery__box')
--- gulpfile.js
+++ gulpfile.js
@@ -36,6 +36,8 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/@fancyapps/ui/dist/fancybox/fancybox.umd.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
